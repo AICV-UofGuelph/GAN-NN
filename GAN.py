2c89a2f754a64375d445249e9a5cdf42d338a7d6
Aidan Holvik <aidan.holvik@gmail.com>
2022-09-01 11:32:59 -0400

    Forgot to include GAN.py in previous commit

--- GAN.py
+++ GAN.py
@@ -6,6 +6,14 @@ import torch.nn.functional as F
 from torch.nn.common_types import _size_2_t
 from torch.nn.modules.utils import _pair
 
+def round_grad(x):
+    # TODO: cite something? (this function is based off of Thor's code)
+
+    x_hard = torch.round(x)
+    x = (x_hard - x.data) + x
+
+    return x
+
 
 class Pad_Conv2d(nn.Module):
     __doc__ = """Pytorch implementation of Keras' 'same' padding for Conv2d layers. Add this just before Conv2d layers to apply the padding."""
@@ -97,8 +105,77 @@ class Critic(nn.Module):
 
         return block
 
+    def _rescale_endpoints(self, curr):
+        scale = (256.0/curr.shape[-2], 256.0/curr.shape[-1])
+
+        ends = torch.zeros((curr.shape[0],1,256,256))
+        for i in range(curr.shape[0]):
+            # Find endpoints
+            coords = torch.argwhere(curr[i,0,:,:] == 2, )
+
+            # Translate endpoints
+            coords[:,0] = coords[:,0] * scale[0]
+            coords[:,1] = coords[:,1] * scale[1]
+
+            # Connect endpoints with ones
+            x1 = coords[0,1].item()
+            x2 = coords[1,1].item()
+            y1 = coords[0,0].item()
+            y2 = coords[1,0].item()
+
+            x = x1
+            y = y1
+
+            if (x1 < x2):
+                x_dir = 1
+            else:
+                x_dir = -1
+
+            if (y1 < y2):
+                y_dir = 1
+            else:
+                y_dir = -1
+
+            # Determine y from x
+            if x2-x1 != 0:
+                m = (y2-y1)/(x2-x1)
+                while x != x2:
+                    y = round_grad(m*(x-x1) + y1)
+                    ends[i,0,y,x] = 1
+                    x += x_dir
+            else:
+                while x != x2:
+                    ends[i,0,y1,x] = 1
+                    x += x_dir
+
+
+            x = x1
+            y = y1
+
+            # Determine x from y
+            if y2-y1 != 0:
+                m = (x2-x1)/(y2-y1)
+                while y != y2:
+                    x = round_grad(m*(y-y1) + x1)
+                    ends[i,0,y,x] = 1
+                    y += y_dir
+            else:
+                while y != y2:
+                    ends[i,0,y,x1] = 1
+                    y += y_dir
+
+            
+            ends[i,0,coords[0,0],coords[0,1]] = 2
+            ends[i,0,coords[1,0],coords[1,1]] = 2
+        return ends
+
 
     def forward(self, x):
+
+        ends = self._rescale_endpoints(x[:,1:2,:,:])    # Rescale endpoints matrix while preserving 1-width path and endpoints
+        x = F.interpolate(x, (x.shape[0],x.shape[1],256,256))   # Rescale input matrix
+        x[:,1:2,:,:] = ends     # replace current endpoints channel with the properly rescaled one
+
         y = self.net(x)
         return y
 
@@ -137,7 +214,7 @@ class Generator(nn.Module):
         # Final output layer
         self.out = nn.Sequential(
             nn.ConvTranspose2d(f[15]+f[1], f[16], 4, 2, 1, device=device),
-            nn.Sigmoid()
+            nn.Tanh()
         )
 
     
@@ -165,8 +242,79 @@ class Generator(nn.Module):
 
         return block
         
+    def _rescale_endpoints(self, curr):
+        scale = (256.0/curr.shape[-2], 256.0/curr.shape[-1])
+
+        ends = torch.zeros((curr.shape[0],1,256,256))
+        for i in range(curr.shape[0]):
+            # Find endpoints
+            coords = torch.argwhere(curr[i,0,:,:] == 2)
+
+            # Translate endpoints
+            coords[:,0] = coords[:,0] * scale[0]
+            coords[:,1] = coords[:,1] * scale[1]
+            # coords = torch.round(coords)
+
+            # Connect endpoints with ones
+            x1 = coords[0,1].item()
+            x2 = coords[1,1].item()
+            y1 = coords[0,0].item()
+            y2 = coords[1,0].item()
+
+            x = x1
+            y = y1
+
+            if (x1 < x2):
+                x_dir = 1
+            else:
+                x_dir = -1
+
+            if (y1 < y2):
+                y_dir = 1
+            else:
+                y_dir = -1
+
+            # Determine y from x
+            if x2-x1 != 0:
+                m = (y2-y1)/(x2-x1)
+                while x != x2:
+                    y = round_grad(m*(x-x1) + y1)
+                    ends[i,0,y,x] = 1
+                    x += x_dir
+            else:
+                while x != x2:
+                    ends[i,0,y1,x] = 1
+                    x += x_dir
+
+
+            x = x1
+            y = y1
+
+            # Determine x from y
+            if y2-y1 != 0:
+                m = (x2-x1)/(y2-y1)
+                while y != y2:
+                    x = round_grad(m*(y-y1) + x1)
+                    ends[i,0,y,x] = 1
+                    y += y_dir
+            else:
+                while y != y2:
+                    ends[i,0,y,x1] = 1
+                    y += y_dir
+
+            
+            ends[i,0,coords[0,0],coords[0,1]] = 2
+            ends[i,0,coords[1,0],coords[1,1]] = 2
+        return ends
+
+
+
+
 
     def forward(self, x):
+        ends = self._rescale_endpoints(x[:,:1,:,:])    # Rescale endpoints matrix while preserving 1-width path and endpoints
+        x = F.interpolate(x, (x.shape[0],x.shape[1],256,256))   # Rescale input matrix
+        x[:,:1,:,:] = ends     # replace current endpoints channel with the properly rescaled one
 
         # encoder
         s1 = self.e1(x)
@@ -218,7 +366,14 @@ class Generator(nn.Module):
 
     def _round(self, mat):
         # TODO: cite something? (this function is based off of Thor's code)
-        mat_hard = torch.round(mat)
-        mat = (mat_hard - mat.data) + mat
 
+        paths = mat[:,:1,:,:]
+        maps = mat[:,-1:,:,:]
+
+        # Confine path to [0,1] then round
+        paths = paths + 1
+        paths = paths / 2
+        paths = round_grad(paths)
+
+        mat = torch.concat((paths, maps), axis=1)
         return mat
